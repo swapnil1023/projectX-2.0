ef4eeadff75aea4d48ab8b2f587bac69d42820b7
swapnil1023 <50006812+swapnil1023@users.noreply.github.com>
2019-09-04 14:27:38 +0530

    fixed the business report activity by fetching the data from firestore

--- app/src/main/java/com/example/projectx/businessReport.java
+++ app/src/main/java/com/example/projectx/businessReport.java
@@ -3,12 +3,23 @@ package com.example.projectx;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import javax.annotation.Nullable;
 
 public class businessReport extends AppCompatActivity {
 
     menu_orders orders;
     TextView totalSell;
+    FirebaseFirestore menu;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -18,14 +29,31 @@ public class businessReport extends AppCompatActivity {
         orders = new menu_orders(this);
         totalSell = findViewById(R.id.totalSell);
 
-        Cursor cursor = orders.getAllOrderData();
-        int total = 0;
-        while(cursor.moveToNext())
-        {
-            total+= Integer.parseInt(cursor.getString(4)) * Integer.parseInt(cursor.getString(2));
-        }
+        menu = FirebaseFirestore.getInstance();
 
-        totalSell.setText(String.valueOf(total));
+        menu.collection("All Orders").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e)
+            {
+                int total = 0;
+
+                if(e!=null)
+                {
+                    Toast.makeText(businessReport.this,"some exception",Toast.LENGTH_SHORT).show();
+                }
+                for(DocumentChange emp: queryDocumentSnapshots.getDocumentChanges())
+                {
+                    if(emp.getType() == DocumentChange.Type.ADDED)
+                    {
+                           total = total +Integer.parseInt(emp.getDocument().get("Total Price").toString());
+
+                    }
+                }
+                totalSell.setText(String.valueOf(total));
+
+            }
+        });
+
 
     }
 }
