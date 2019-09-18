d4cb647e8e81cf5a04319c367254c5c7d875a366
swapnil1023 <50006812+swapnil1023@users.noreply.github.com>
2019-09-18 17:14:30 +0530

    added the progress bar in employee pass change. Although there's a slight problem with the activity.

--- app/src/main/java/com/example/projectx/changeEmpPass.java
+++ app/src/main/java/com/example/projectx/changeEmpPass.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,6 +27,7 @@ public class changeEmpPass extends AppCompatActivity {
     TextView newPass2;
     Button change;
     Button clear;
+    ProgressBar prog;
     FirebaseFirestore employee;
     DocumentReference empRef;
     @Override
@@ -44,6 +46,7 @@ public class changeEmpPass extends AppCompatActivity {
         newPass= findViewById(R.id.empNewPass);
         newPass2= findViewById(R.id.empNewPass2);
         oldPass= findViewById(R.id.empOldPass);
+        prog = findViewById(R.id.prog6);
 
         change.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +56,7 @@ public class changeEmpPass extends AppCompatActivity {
                     Toast.makeText(changeEmpPass.this,"Please enter all the fields",Toast.LENGTH_SHORT).show();
                 else
                 {
+                    prog.setVisibility(View.VISIBLE);
                     empRef.get()
                             .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                                 @Override
@@ -67,12 +71,19 @@ public class changeEmpPass extends AppCompatActivity {
 
                                             empRef.update(passMap);
                                             Toast.makeText(changeEmpPass.this, "Password Changed", Toast.LENGTH_SHORT).show();
+                                            prog.setVisibility(View.INVISIBLE);
                                         }
                                         else
+                                        {
                                             Toast.makeText(changeEmpPass.this,"The Re-entered Password Doesn't Match The New Password",Toast.LENGTH_SHORT).show();
+                                            prog.setVisibility(View.INVISIBLE);
+                                        }
                                     }
                                     else
+                                    {
                                         Toast.makeText(changeEmpPass.this,"Incorrect old password",Toast.LENGTH_SHORT).show();
+                                        prog.setVisibility(View.INVISIBLE);
+                                    }
                                 }
                             })
                             .addOnFailureListener(new OnFailureListener() {
